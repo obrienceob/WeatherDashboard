554200cf33d7a097ee8b89cc399aca9dde0d1a74
Conor O'Brien <obrienceob@gmail.com>
2021-02-01 20:11:55 -0800

    wq :wq :wq '

--- assets/script.js
+++ assets/script.js
@@ -42,7 +42,7 @@ searchButton.click(function () {
             console.log(response.dt);
             console.log(date);
             currentCity.append(response.name + " " + date);
-            currentCity.append('<img src="https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png)">');
+            currentCity.append(`<img src="https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png">`);
             var cityTemperature = currentCity.append("<p>");
             currentCity.append(cityTemperature);
             cityTemperature.append("<p>" + "Temperature: " + response.main.temp + "</p>");
@@ -56,7 +56,7 @@ searchButton.click(function () {
             method: 'GET'
             }).then(function (response) {
                 var currentUV = cityTemperature.append("<p>" + "UV Index: " + response.current.uvi + "</p>").addClass("card-text");
-                currentUV.addClass("UV");
+                //currentUV.addClass("UV");
                 cityTemperature.append(currentUV)
             });
         });
